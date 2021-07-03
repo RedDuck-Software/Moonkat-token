8baae36a9146268c759200000aa15502667a2393
kostyamospan <kostya04042001@gmail.com>
2021-07-03 17:17:46 +0300

    claimTokens

--- contracts/PreSaleClaimer.sol
+++ contracts/PreSaleClaimer.sol
@@ -6,6 +6,13 @@ import "./MoonKat.sol";
 contract PreSaleClaimer is Ownable{ 
     using SafeMath for uint256;
 
+    struct ClaimerInfo {
+        uint256 totalTokensAmount;
+        uint256 periodPaymentAmount; 
+        uint256 paymentsMade;
+        bool isValue;
+    }
+
     uint256 public constant unFreezePeriod = 1 days;
     
     uint256 public constant unFreezePercentage = 2; // 2%
@@ -30,7 +37,26 @@ contract PreSaleClaimer is Ownable{
     
 
     function claimTokens() public { 
+        ClaimerInfo storage senderInfo = tokenClaimers[msg.sender];
 
+        require(claimAvailableFrom < block.timestamp, "Claiming is not started yet");
+        require(senderInfo.isValue, "Address is not in the claim list");
+        require(senderInfo.paymentsMade
+            .mul(senderInfo.periodPaymentAmount) < senderInfo.totalTokensAmount,
+            "All tokens is already withdrawed");
+
+        uint256 passedPeriodPaymentsCount = _calculatePassedPeriodPaymentsCount();
+
+        require( passedPeriodPaymentsCount > senderInfo.paymentsMade, 
+          "No unfreezed tokens available for now");
+        
+        uint256 tokensToSend = passedPeriodPaymentsCount.sub(senderInfo.paymentsMade).mul(senderInfo.periodPaymentAmount);
+
+        senderInfo.paymentsMade = passedPeriodPaymentsCount;
+
+        mkatToken.transfer(msg.sender, tokensToSend);
+        
+        tokenClaimers[msg.sender] = senderInfo;
     }
 
 
@@ -40,7 +66,7 @@ contract PreSaleClaimer is Ownable{
 
 
     function _addTokenClaimer(address _claimerAddress, uint256 _tokensAmount) private { 
-        require(!tokenClaimers[_claimerAddress].isValue, "Address is already in the list");
+        require(!tokenClaimers[_claimerAddress].isValue, "Address is already in the calim list");
 
         tokenClaimers[_claimerAddress] = ClaimerInfo(_tokensAmount, _calculatePeriodPaymentAmount(_tokensAmount, percentageBasicPoints), 0, true);
         claimers.push(_claimerAddress);
@@ -48,7 +74,7 @@ contract PreSaleClaimer is Ownable{
 
 
     function _calculatePeriodPaymentAmount(uint256 _totalAmount, uint256 _basicPoints) private pure returns (uint256){ 
-        return _totalAmount.mul(unFreezePercentage.mul(_basicPoints)).div(100 * _basicPoints);
+        return _totalAmount.mul(unFreezePercentage.mul(_basicPoints)).div(uint256(100).mul(_basicPoints));
     }
 
     function _calculateUnFreezeAmount(address _claimerAddress) private view { 
@@ -58,19 +84,11 @@ contract PreSaleClaimer is Ownable{
     function _calculatePassedPeriodPaymentsCount() private view returns (uint256){ 
         require(claimAvailableFrom < block.timestamp);
 
-        return claimAvailableFrom.sub(block.timestamp).div(1 days);
+        return claimAvailableFrom.sub(block.timestamp).div(unFreezePeriod);
     }
 
 
     function _hardcodeAddresses() private {
 
-    }
-
-
-    struct ClaimerInfo {
-        uint256 totalTokensAmount;
-        uint256 periodPaymentAmount; 
-        uint256 paymentsMade;
-        bool isValue;
     }
 }
